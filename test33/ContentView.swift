77b298da7a08a87a1939f3a37c21998ac603edbd
Rano0ody <rand45042@gmail.com>
2025-04-09 16:30:31 +0300

    Initial Commit

---
+++ test33/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test33
+//
+//  Created by Rand abdullatif on 11/10/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test33/test33App.swift
@@ -0,0 +1,17 @@
+//
+//  test33App.swift
+//  test33
+//
+//  Created by Rand abdullatif on 11/10/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct test33App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
